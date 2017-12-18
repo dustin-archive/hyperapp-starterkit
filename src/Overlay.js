f8f2022fc9913375eca3c08ef5c0ef4a08730c90
Dustin Dowell <dustindowell22@gmail.com>
2017-12-18 16:59:31 -0600

    new lines

--- src/Overlay.js
+++ src/Overlay.js
@@ -3,13 +3,17 @@ var Overlay = {
   init: function (data) {
     return function (state) {
       if (!data.target.classList.contains('_overlay') && state.overlay != null) {
-        return { overlay: null }
+        return {
+          overlay: null
+        }
       }
     }
   },
   toggle: function (data) {
     return function (state) {
-      return { overlay: state.overlay === data ? null : data }
+      return {
+        overlay: state.overlay === data ? null : data
+      }
     }
   }
 }
