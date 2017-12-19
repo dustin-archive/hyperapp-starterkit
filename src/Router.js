0bd471af24e875428ed7874e65327fb118bcb096
Dustin Dowell <dustindowell22@gmail.com>
2017-12-18 18:50:25 -0600

    add a callback to init

--- src/Router.js
+++ src/Router.js
@@ -2,13 +2,15 @@
 import { encode, decode } from '@whaaaley/query-string'
 
 var Router = {
-  init: function () {
+  init: function (data) {
     var hash = window.location.hash
     var index = hash.indexOf('?')
-    return {
+    var state = {
       query: decode(hash.slice(index)),
       path: hash.slice(1, index === -1 ? hash.length : index)
     }
+    data && data(state)
+    return state
   },
   route: function (data) {
     return function (state) {
